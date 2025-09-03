531e8e087acec04a8959cfad99c64b028750f9ca
bhumipandey06 <bhumipandey1188@gmail.com>
2025-09-03 17:40:07 +0530

    implemented http file to conect clerk with db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div> 
-      <SignedOut>
-        <SignUpButton>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-
-      <UserButton/>
-
-      <SignedIn>
-        <SignOutButton>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            Logout
-          </button>
-        </SignOutButton>
-      </SignedIn>
+      
     </div>
   );
 }
